20b7b39ab826553a64acbfed4d39ec755b689696
EstebanForge <1696145+EstebanForge@users.noreply.github.com>
2026-03-30 09:00:34 -0300

    better testeable entrypoint

--- wicket-wp-portus.php
+++ wicket-wp-portus.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Wicket Portus
  * Plugin URI:  https://wicket.io
@@ -7,22 +8,101 @@
  * Author:      Wicket Inc.
  * Author URI:  https://wicket.io
  * Text Domain: wicket-portus
- * Requires PHP: 8.2
+ * Requires PHP: 8.2.
  */
 
 declare(strict_types=1);
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-define( 'WICKET_PORTUS_VERSION', '0.1.0' );
-define( 'WICKET_PORTUS_DIR', plugin_dir_path( __FILE__ ) );
-define( 'WICKET_PORTUS_URL', plugin_dir_url( __FILE__ ) );
-define( 'WICKET_PORTUS_FILE', __FILE__ );
+define('WICKET_PORTUS_VERSION', get_file_data(__FILE__, ['Version' => 'Version'])['Version']);
+define('WICKET_PORTUS_DIR', plugin_dir_path(__FILE__));
+define('WICKET_PORTUS_URL', plugin_dir_url(__FILE__));
+define('WICKET_PORTUS_FILE', __FILE__);
 
-if ( file_exists( WICKET_PORTUS_DIR . 'vendor/autoload.php' ) ) {
-	require_once WICKET_PORTUS_DIR . 'vendor/autoload.php';
+add_action(
+    'plugins_loaded',
+    [Wicket_Portus_Bootstrap::get_instance(), 'plugin_setup'],
+    99
+);
+
+final class Wicket_Portus_Bootstrap
+{
+    /**
+     * Plugin instance.
+     *
+     * @var self|null
+     */
+    protected static ?self $instance = null;
+
+    /**
+     * URL to this plugin directory.
+     *
+     * @var string
+     */
+    public string $plugin_url = '';
+
+    /**
+     * Absolute path to this plugin directory.
+     *
+     * @var string
+     */
+    public string $plugin_path = '';
+
+    /**
+     * Intentionally empty constructor.
+     */
+    public function __construct() {}
+
+    /**
+     * Access this plugin's working instance.
+     *
+     * @return self
+     */
+    public static function get_instance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Regular plugin setup.
+     *
+     * @return void
+     */
+    public function plugin_setup(): void
+    {
+        $this->plugin_url = plugins_url('/', __FILE__);
+        $this->plugin_path = plugin_dir_path(__FILE__);
+        $this->load_language('wicket-portus');
+
+        $autoload = $this->plugin_path . 'vendor/autoload.php';
+        if (file_exists($autoload)) {
+            require_once $autoload;
+        }
+
+        if (class_exists('\WicketPortus\Plugin')) {
+            WicketPortus\Plugin::get_instance();
+        }
+    }
+
+    /**
+     * Loads translation file.
+     *
+     * @param string $domain
+     * @return void
+     */
+    public function load_language(string $domain): void
+    {
+        load_plugin_textdomain(
+            $domain,
+            false,
+            $this->plugin_path . 'languages'
+        );
+    }
 }
-
-add_action( 'plugins_loaded', [ \WicketPortus\Plugin::class, 'get_instance' ], 99 );
